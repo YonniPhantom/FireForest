37f5fd23cabb2fee7b096a0b43079cc777524ab2
Jonathan Herrera <yonniphantom@gmail.com>
2024-06-19 16:40:32 -0600

    Login

--- src/screens/Auth/Login.jsx
+++ src/screens/Auth/Login.jsx
@@ -1,13 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React, { useState } from 'react'
 
 const Login = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Inicia Sesión</Text>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    </View>
-  )
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Inicia Sesión</Text>
+
+            <View>
+                <View style={styles.section}>
+                    <Text style={styles.subtitle}>Correo Electrónico</Text>
+                    <TextInput style={styles.input} placeholder="example@domain.com" value={email} onChangeText={setEmail} />
+                </View>
+            </View>
+        </View>
+    )
 }
 
 export default Login
@@ -16,10 +26,25 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     title: {
         fontSize: 26,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginBottom: 50
+    },
+    section: {
+        width: 300,
+        height: 50
+    },
+    subtitle: {
+        color: 'gray',
+        fontWeight: 'bold',
+        marginBottom: 10
+    },
+    input: {
+        backgroundColor: 'lightgray',
+        borderRadius: 15,
+        height: 40
     }
 })
