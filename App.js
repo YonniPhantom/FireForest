067cfb0c591cfbf2e2f842fc2c647b5dd7f4da6f
Jonathan Herrera <yonniphantom@gmail.com>
2024-06-19 18:43:52 -0600

    Splashscreen e iconos

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import { CredentialsContext } from './src/Components/CredentialsContext';
 import RootStack from './src/navigation/RootStack';
-import react, { useState } from 'react';
+import react, { useState, useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync()
+  .catch(e => console.log(e));
 
 export default function App() {
   const [storedCredentials, setStoredCredentials] = useState("");
+
+  useEffect(() => {
+    setTimeout(async () => {
+      await SplashScreen.hideAsync();
+    }, 1500)
+  })
 
   return (
     <CredentialsContext.Provider value={{storedCredentials, setStoredCredentials}}>
